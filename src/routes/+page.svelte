6d619564b8ce9740afec87fff9117113170ad334
neoan <stefan.roehrl@gmail.com>
2024-03-26 14:13:19 -0400

    frontpage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -110,7 +110,7 @@
 </style>
 <div class="w-full banner"></div>
 
-<div class="w-4/5 lg:max-w-screen-lg mx-auto mb-24">
+<div class="md:w-4/5 px-2 lg:max-w-screen-lg mx-auto mb-24">
     <section>
         <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
 
@@ -118,85 +118,76 @@
                 Wir bieten:</p>
             <h1 class="mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-700 md:text-4xl lg:text-5xl dark:text-white">
                 Den flexibelsten Nebenjob der Stadt</h1>
-            <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Flexibel,
-                jung & dynamisch: Das ist recrew. </p>
+            <p class="font-semibold mb-2">
+                Täglich neue Jobs und Aufgaben als Teil einer familiären Crew meistern:
+            </p>
+            <p>
+                Werde ein Teil unserer Crew und bewirb dich noch heute bei uns! Wir freuen uns auf deine (Schnell)
+                Bewerbung.
+            </p>
         </div>
-        <div class="grid grid-cols-3 gap-3">
-            <div class="col-span-3 lg:col-span-1 dark:text-white">
-                <div class="hidden md:flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                    <img class="w-10 h-10 inline" src="{base}/apple-touch-icon.png" alt="CR">
-                </div>
-                <p class="font-semibold mb-2">
-                    Täglich neue Jobs und Aufgaben als Teil einer familiären Crew meistern:
-                </p>
-                <p>
-                    Werde ein Teil unserer Crew und bewirb dich noch heute bei uns! Wir freuen uns auf deine (Schnell)
-                    Bewerbung.
-                </p>
+        <div class="rounded-lg shadow shadow-primary-400 py-3 px-4">
+            <Heading class="text-neutral-600" tag="h3">Schnellbewerbung</Heading>
+            <form bind:this={form} on:submit|preventDefault={submit} class="mt-4">
+                <div class="grid gap-6 mb-6 md:grid-cols-2">
+                    <div>
+                        <Label for="first_name" class="mb-2">Vorname</Label>
+                        <Input bind:value={candidate.firstname} type="text" id="first_name" placeholder="Max" required />
+                    </div>
+                    <div>
+                        <Label for="last_name" class="mb-2">Nachname</Label>
+                        <Input bind:value={candidate.lastname} type="text" id="last_name" placeholder="Mustermann" required />
+                    </div>
+                    <div>
+                        <Label for="email" class="mb-2">E-Mail</Label>
+                        <Input bind:value={candidate.email} type="email" id="email" placeholder="max.mustermann@googlemail.com" required />
+                    </div>
+                    <div>
+                        <Label for="mobile" class="mb-2">Handynummer</Label>
+                        <Input bind:value={candidate.mobile} pattern={'[\\+]\\d{1,3}\\s*\\d{3,4}\\s*\\d{7}'} type="text" id="mobile" placeholder="+49 161 123456" required />
+                    </div>
+                    <div>
+                        <Label for="region" class="mb-2">Region</Label>
+                        <Select bind:value={candidate.region} id="region" required items={regions} />
+                    </div>
+                    <div>
+                        <Label for="application_field" class="mb-2">Einsatzbereich</Label>
+                        <Select bind:value={candidate.application_field} id="application_field" required items={application_fields} />
+                    </div>
+                    <div>
+                        <Label for="letter_motivation" class="mb-2">Du willst ins Team weil...</Label>
+                        <Textarea minlength="5" maxlength="700" bind:value={candidate.letter_motivation} rows="9" id="letter_motivation" required />
+                        <Helper class="mt-1" color={(candidate.letter_motivation.length < 5 || candidate.letter_motivation.length > 700)  ? 'red' : 'green'}>{candidate.letter_motivation.length}/700</Helper>
+                    </div>
 
-            </div>
-            <div class="col-span-3 lg:col-span-2 rounded-lg shadow shadow-primary-400 py-3 px-4">
-                <Heading class="text-neutral-600" tag="h3">Schnellbewerbung</Heading>
-                <form bind:this={form} on:submit|preventDefault={submit} class="mt-4">
-                    <div class="grid gap-6 mb-6 md:grid-cols-2">
-                        <div>
-                            <Label for="first_name" class="mb-2">Vorname</Label>
-                            <Input bind:value={candidate.firstname} type="text" id="first_name" placeholder="Max" required />
-                        </div>
-                        <div>
-                            <Label for="last_name" class="mb-2">Nachname</Label>
-                            <Input bind:value={candidate.lastname} type="text" id="last_name" placeholder="Mustermann" required />
-                        </div>
-                        <div>
-                            <Label for="email" class="mb-2">E-Mail</Label>
-                            <Input bind:value={candidate.email} type="email" id="email" placeholder="max.mustermann@googlemail.com" required />
-                        </div>
-                        <div>
-                            <Label for="mobile" class="mb-2">Handynummer</Label>
-                            <Input bind:value={candidate.mobile} pattern={'[\\+]\\d{1,3}\\s*\\d{3,4}\\s*\\d{7}'} type="text" id="mobile" placeholder="+49 161 123456" required />
-                        </div>
-                        <div>
-                            <Label for="region" class="mb-2">Region</Label>
-                            <Select bind:value={candidate.region} id="region" required items={regions} />
-                        </div>
-                        <div>
-                            <Label for="application_field" class="mb-2">Einsatzbereich</Label>
-                            <Select bind:value={candidate.application_field} id="application_field" required items={application_fields} />
-                        </div>
-                        <div>
-                            <Label for="letter_motivation" class="mb-2">Du willst ins Team weil...</Label>
-                            <Textarea minlength="5" maxlength="700" bind:value={candidate.letter_motivation} rows="9" id="letter_motivation" required />
-                            <Helper class="mt-1" color={(candidate.letter_motivation.length < 5 || candidate.letter_motivation.length > 700)  ? 'red' : 'green'}>{candidate.letter_motivation.length}/700</Helper>
-                        </div>
-
-                        <div class="mt-2">
-                            <Dropzone on:drop={dropHandle}
-                                      on:dragover={(event) => {
+                    <div class="mt-2">
+                        <Dropzone on:drop={dropHandle}
+                                  on:dragover={(event) => {
                                         event.preventDefault();
                                       }}
-                                      on:change={handleChange}>
-                                {#if !candidate.photo}
-                                    <UserCircleSolid class="w-12 h-12 text-primary-800/70 dark:text-white"/>
-                                    <Heading class="text-neutral-600" tag="h4">Bild hochladen</Heading>
-                                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Klicken</span> oder drag & drop</p>
-                                    <p class="text-xs text-gray-500 dark:text-gray-400">PNG oder JPG</p>
-                                {:else}
-                                    <img class="max-h-full max-w-full" src={preview} alt="profile-pic">
-                                {/if}
+                                  on:change={handleChange}>
+                            {#if !candidate.photo}
+                                <UserCircleSolid class="w-12 h-12 text-primary-800/70 dark:text-white"/>
+                                <Heading class="text-neutral-600" tag="h4">Bild hochladen</Heading>
+                                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Klicken</span> oder drag & drop</p>
+                                <p class="text-xs text-gray-500 dark:text-gray-400">PNG oder JPG</p>
+                            {:else}
+                                <img class="max-h-full max-w-full" src={preview} alt="profile-pic">
+                            {/if}
 
-                            </Dropzone>
-                        </div>
-
+                        </Dropzone>
                     </div>
-                    <div class="grid gap-3">
-                        <Botr bind:valid/>
-                        <Checkbox required>Ich bin mit der Verarbeitung meiner Daten einverstanden <span class="text-xs pl-2">(<a class="visited:text-blue-600 hover:text-blue-600 text-blue-500" rel="nofollow noopener" target="_blank" href="https://www.recrew.info/kopie-von-dsgvo">Datenschutzerklärung</a>)</span></Checkbox>
-                        <Button disabled={!valid} type="submit">BEWERBEN</Button>
 
-                    </div>
-                </form>
-            </div>
+                </div>
+                <div class="grid gap-3">
+                    <Botr bind:valid/>
+                    <Checkbox required>Ich bin mit der Verarbeitung meiner Daten einverstanden <span class="text-xs pl-2">(<a class="visited:text-blue-600 hover:text-blue-600 text-blue-500" rel="nofollow noopener" target="_blank" href="https://www.recrew.info/kopie-von-dsgvo">Datenschutzerklärung</a>)</span></Checkbox>
+                    <Button disabled={!valid} type="submit">BEWERBEN</Button>
+
+                </div>
+            </form>
         </div>
+
     </section>
 </div>
 
