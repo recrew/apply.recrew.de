3e15dd7fb9885acf1d38f7b044fe496031ff90cd
brivera <rrivera@redbannermedia.com>
2026-02-27 04:32:05 -0500

    Majority of Swiss Id working

--- src/lib/utils/readPassport.ts
+++ src/lib/utils/readPassport.ts
@@ -1,3 +1,4 @@
+import { date } from "svelte-i18n";
 import { capitalize } from "./caseConverter";
 
 let lastName = "";
@@ -81,28 +82,37 @@ export const readIdFrontCard = (parsedText: string) => {
     const lines = parsedText.split("\n");
     lastLine = "";
     lines.forEach((line) => {
+        console.log(lastLine, "lastLine");
+
         if (line.includes("DEUTSCHLAND") || lastLine.includes("Dokument")) {
             let number: string[] = line.split("\t");
             idNumber = number.length > 2 ? number[2] : number[0];
         } else if (
             lastLine.includes("IDENTITY CARD / CARTE D'IDENTITE") ||
-            lastLine.includes("Vorname")
+            lastLine.includes("Surname")
         ) {
-            lastName = capitalize(line.trim());
+            lastName = line.includes("+")
+                ? capitalize(line.split("+")[0])
+                : capitalize(line.trim());
+
             maidenName = lastName;
-        } else if (lastLine.includes("Vornamen")) {
-            firstName = capitalize(line.trim());
+        } else if (lastLine.includes("Vornamen") || lastLine.includes("Vorname")) {
+            firstName = capitalize(line.replace(/-/g, " ").trim());
         } else if (
             lastLine.includes("Date de naissance") ||
             lastLine.includes("Date of birth")
         ) {
-            dateOfBirth = line.match(/\d{2}\.\d{2}\.\d{4}/)?.[0] ?? "";
+            dateOfBirth = line.includes(".")
+                ? (line.match(/\d{2}\.\d{2}\.\d{4}/)?.[0] ?? "")
+                : line.replace(" ", ".");
         } else if (lastLine.includes("Geburtsort")) {
             placeOfBirth = capitalize(line.trim());
         } else if (line.includes("Nationality") && !firstName) {
             firstName = capitalize(lastLine.trim());
         } else if (lastLine.includes("[a]")) {
             maidenName = capitalize(line.trim().match(/[A-Z-\s]{3,}/)?.[0] ?? "");
+        } else if (line.includes("IDENTITÄTSKARTE") && !idNumber) {
+            idNumber = line.split("\t")[1].trim().toUpperCase();
         }
         lastLine = line;
     });
